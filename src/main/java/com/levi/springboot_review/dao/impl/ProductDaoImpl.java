cbb23a584e4e93679af04b77df99e44e52bbafd9
lsulevi <qqqqban@gmail.com>
2025-05-18 22:48:27 +0800

    商品功能-實作查詢商品列表(排序)

--- src/main/java/com/levi/springboot_review/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/levi/springboot_review/dao/impl/ProductDaoImpl.java
@@ -39,6 +39,9 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name like :search";
             map.put("search", "%" + productQueryParams.getSearch() + "%" );
         }
+
+        sql = sql + " order by " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
         List<Product> productList = namedParameterJdbcTemplate.query(sql,map,new ProductRowMapper());
 
         return productList;
