9b9a6b713b67005e8ccf7e44761b30becc5e250b
lsulevi <qqqqban@gmail.com>
2025-05-18 15:11:56 +0800

    商品功能-實作查詢商品列表

--- src/main/java/com/levi/springboot_review/dao/ProductDao.java
+++ src/main/java/com/levi/springboot_review/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.levi.springboot_review.dao;
 import com.levi.springboot_review.dto.ProductRequest;
 import com.levi.springboot_review.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+     List<Product> getProducts();
 
      Product getProductById(Integer productId);
 
--- src/main/java/com/levi/springboot_review/service/ProductService.java
+++ src/main/java/com/levi/springboot_review/service/ProductService.java
@@ -3,7 +3,11 @@ package com.levi.springboot_review.service;
 import com.levi.springboot_review.dto.ProductRequest;
 import com.levi.springboot_review.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
